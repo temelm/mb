71f4e895b0d600451f778772d38461606fb8113b
Mustafa Temel <mustafatemel90@gmail.com>
2019-01-20 21:53:26 +0000

    Project skeleton.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div id='app' />
+    )
+  }
+}
+
+export default App
