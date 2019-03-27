6fcedd876ac707b8bbb8198857df2ad5790af1d9
Mustafa Temel <mustafatemel90@gmail.com>
2019-03-27 00:47:39 +0000

    Added PHP mail logic. Receipient e-mail address and Subject line text to be updated.

---
+++ src/server/mail.php
@@ -0,0 +1,31 @@
+<?php
+  function send () {
+    $result = 'error';
+
+    if (isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['email']) && !empty($_POST['email'])
+      && isset($_POST['message']) && !empty($_POST['message'])) {
+      $name = strip_tags($_POST['name']);
+      $email = strip_tags($_POST['email']);
+      $company = '';
+      if (isset($_POST['company']) && !empty($_POST['company'])) {
+        $company = '<p>Company: '.strip_tags($_POST['company']).'</p>';
+      }
+      $to = 'mustafatemel90@gmail.com'; // @todo
+      $subject = 'Message from '.$name.' via temelm.com'; // @todo
+      $message = '<p>Name: '.$name.'</p>'.'<p>Email: '.$email.'</p>'.$company.'<p>'.strip_tags($_POST['message']).'</p>';
+      $message = wordwrap($message, 120);
+      $headers = 'From: '.$email."\r\n";
+      $headers .= 'Reply-To: '.$email."\r\n";
+      $headers .= 'MIME-Version: 1.0'."\r\n";
+      $headers .= 'Content-Type: text/html; charset=UTF-8'."\r\n";
+
+      mail($to, $subject, $message, $headers);
+      
+      $result = 'success';
+    }
+
+    return $result;
+  }
+  
+  echo send();
+?>
