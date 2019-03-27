a50ccd4e2e39fbe5223a0946b93124bbd8365f72
Mustafa Temel <mustafatemel90@gmail.com>
2019-03-27 00:52:59 +0000

    Added FontAwesom LinkedIn logo dependency.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
 import './styles/index.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import App from './components/App'
+
+library.add(faBars, faLinkedinIn)
 
 ReactDOM.render(<App />, document.getElementById('root'))
