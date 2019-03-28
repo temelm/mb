3a9c13f78fe23b07b71215e0e5a15ea0fc88608b
Mustafa Temel <mustafatemel90@gmail.com>
2019-03-28 00:40:06 +0000

    Added Welcome component.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,9 +4,9 @@ const Welcome = () => (
   <section id='welcome'>
     <div className='wrapper'>
       <h1>Welcome to MB Accounting &amp; Consulting Ltd</h1>
-      <h4>
+      <p>
         <q>Accounting simplified.</q>
-      </h4>
+      </p>
     </div>
   </section>
 )
