b6573d134c0b07f7306db10104789d1a92d91c82
Mustafa Temel <mustafatemel90@gmail.com>
2019-03-28 01:07:04 +0000

    Added Nav, Welcome, and About sections.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,40 @@
 import React from 'react'
+import Nav from './Nav'
 import Welcome from './Welcome'
+import About from './About'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    this.state = {
+      hamburgerToggle: false
+    }
+    this.setHamburgerToggle = this.setHamburgerToggle.bind(this)
+    this.handleHamburgerToggle = this.handleHamburgerToggle.bind(this)
+  }
+
+  setHamburgerToggle (toggle) {
+    this.setState(prevState => ({
+      hamburgerToggle: (typeof toggle === 'boolean') ? toggle : !prevState.hamburgerToggle
+    }))
+  }
+
+  handleHamburgerToggle (event) {
+    event.preventDefault()
+    this.setHamburgerToggle()
   }
 
   render () {
+    const { hamburgerToggle } = this.state
     return (
       <div id='app'>
+        <Nav
+          hamburgerToggle={hamburgerToggle}
+          setHamburgerToggle={this.setHamburgerToggle}
+          handleHamburgerToggle={this.handleHamburgerToggle}
+        />
         <Welcome />
+        <About />
       </div>
     )
   }
