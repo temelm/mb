6ecea1a52a23d90b6fd76417175468d3db383fa0
Mustafa Temel <mustafatemel90@gmail.com>
2019-04-03 00:15:12 +0100

    Minor refactor.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import App from './components/App'
+import './styles/index.scss'
 
 library.add(faBars, faLinkedinIn)
 
